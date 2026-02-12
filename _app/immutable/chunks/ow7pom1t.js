267f73f71793e4495b6771de31ba8aa1a0fdd083
SamWieHTWG <wiertzsamuel@gmail.com>
2026-02-12 12:26:59 +0100

    Deploy update 2026-02-12 12:26

--- _app/immutable/chunks/ow7pom1t.js
+++
@@ -1,1 +0,0 @@
-import{k as a}from"./B9wwaq4J.js";a();
---
+++ _app/immutable/nodes/4.D1MU3O3P.js
@@ -0,0 +1,39 @@
+import{a as r,f as t}from"../chunks/DXdPmsRX.js";import"../chunks/Ci5-XOkP.js";import{g as s,n as a,i as l}from"../chunks/DYRlNsVR.js";import{H as d}from"../chunks/DLj-LQMI.js";var u=t(`<main><!> <section class="intro"><div class="intro-text"><h1>Unser Parklet</h1> <p class="tagline">Ein grüner Ort der Begegnung mitten im Leonhardsviertel.</p></div> <div class="intro-image"><img src="/B3.jpg" alt="Parklet Katharinenstraße"/></div></section> <section class="content"><div class="block"><h2>Konzept</h2> <p>Dank der zweimaligen und großzügigen Förderung des Bezirksrat Mitte konnten wir,
+        eine Gruppe junger Menschen aus dem Leonhardsviertel, nun schon im zweiten Jahr
+        das Parklet in der Katharinenstraße gemeinschaftlich aufbauen, verbessern und mit
+        der Nachbar*innenschaft nutzen.</p> <p>Das Parklet und die Möglichkeit, sich zwischen Autos, Straßen und kommerziellen
+        Angeboten zu entspannen und zu treffen, wurde im letzten Jahr sehr gut
+        angenommen. Mit Liegefläche, Schattenplätzen und Begrünung aus kleinem Gehölz,
+        Stauden und Blumen war das Parklet wie eine kleine grüne Oase mitten in der Stadt.</p></div> <div class="image-row"><img src="/B1.jpg" alt="Parklet Impression"/> <img src="/B5.jpg" alt="Parklet Impression"/></div> <div class="block"><p>Wir würden uns sehr freuen, wenn das Parklet dieses Jahr wieder aufgebaut werden
+        kann und so ein Mehrwert für unser Viertel geschaffen werden kann. Dafür hoffen wir
+        auf Ihre Befürwortung unseres künftigen Antrags bei der Straßenverkehrsbehörde.</p> <p>Der Schwerpunkt für dieses Jahr besteht darin, die verschiedenen Gruppen, die
+        bereits das Parklet nutzen, besser in Austausch treten zu lassen. Dafür wollen wir
+        ganz niederschwellig zu Nachbar*innenschaftstreffen auf die Fläche einladen und uns
+        im öffentlichen Raum begegnen.</p> <p>Mit den Förderungen aus dem letzten Jahr konnten weitere Sitzmöbel und
+        Pflanzkästen gebaut werden sowie eine vielfältige und klimaresistente Begrünung
+        des Parklets angeschafft werden.</p></div> <div class="image-full"><img src="/B2.jpg" alt="Parklet Begrünung"/></div> <div class="block"><h2>Wer wir sind</h2> <p>Wir sind eine Gruppe aus Bewohner*innen des Leonhardsviertels, die sich bewusst für
+        die nachhaltige und gemeinschaftsorientierte Gestaltung des öffentlichen Raums
+        einsetzt. Im Sommer 2020 durften wir Dank der Wanderbaumallee eine Idee davon
+        bekommen, was es heißt gemeinschaftlich und in geteilter Verantwortung die
+        Katharinenstraße zu beleben und in Austausch mit unserer Nachbar*innenschaft zu
+        kommen. Dann lernten wir das Konzept der Parklets kennen und sind nach wie vor
+        überzeugt davon.</p></div> <div class="image-row"><img src="/B4.jpg" alt="Parklet Gemeinschaft"/> <img src="/B6.jpg" alt="Parklet Nachbarschaft"/></div> <div class="block"><h2>Warum ein Parklet im Leonhardsviertel?</h2> <p>Zwischen den engen Gassen, Autos, Gastro-Angeboten und betonierten Flächen gibt
+        es nur wenige Orte, die einfach Aufenthaltsqualität und Schatten bieten. Im Viertel
+        wohnt eine sehr diverse Stadtgesellschaft, doch es herrscht wenig Austausch auf
+        den Straßen und Plätzen. Natürlich können wir mit einem einzelnen Parklet keine
+        klimagerechte Stadt schaffen, doch ist es vielleicht ein kleines Sandkorn in diesem
+        Prozess und wir würden uns freuen, einen Teil dazu beizutragen.</p></div> <div class="image-full"><img src="/B7.jpg" alt="Parklet im Viertel"/></div> <div class="block"><h2>Aufbau</h2> <p>Bis auf kleine Verbesserungen wie das Anbringen von noch mehr Hinweisschildern,
+        das Bauen kleiner Hocker und die Auffrischung der Begrünung soll es in diesem Jahr
+        keine weiteren baulichen Maßnahmen geben. Wir durften in den letzten zwei Jahren
+        erfahren, dass es viel Kapazitäten braucht, um ein Parklet richtig zu pflegen und dieser
+        Aufgabe möchten wir dieses Jahr noch mehr Aufmerksamkeit schenken.</p> <p class="subheading">Der Aufbau soll wie folgt bleiben:</p> <ul><li>Auf der linken Seite soll es einen Sitzbereich mit großem Tisch zum gemeinsamen Essen, Gestalten und Treffen geben.</li> <li>Tarps und Begrünung schützen vor Sonne, Lärm und erhöhen die Aufenthaltsqualität.</li> <li>Der mittlere Teil des Parklets wird dominiert durch ein niedriges Podest, das als Liegefläche dient.</li> <li>Auf der rechten Seite lädt das Tauschregal mit Büchern zum ersten Schritt aufs Parklet und zur Interaktion ein.</li> <li>Das Lastenrad "Rosa" findet hier seine Verleihstation und kann über Lastenrad Stuttgart auf Spendenbasis gebucht werden.</li> <li>Die Kreidetafel lädt zur Kommunikation ein und kann gleichzeitig als Schwarzes Brett dienen.</li> <li>Hinweisschilder vermitteln den Zweck von Parklets und weisen auf einige Regeln zur Nutzung hin.</li> <li>Die digitale Vermittlung läuft über den gut genutzten Instagram Account des Parklets weiter.</li></ul></div> <div class="block"><h2>Sicherheit</h2> <p>Die Gestaltung des Parklets entspricht gewissen Sicherheitsansprüchen. Verstellbare
+        Balken garantieren eine gerade Fläche, die den geregelten Abfluss des Regenwassers
+        gewährleistet. Durch die Absicherung mit Bänken, Podesten, fest integrierten
+        Blumenkübeln und Querstreben an den drei Außenseiten zur Straße hin ist ein Eintreten
+        nur vom Gehweg möglich. Reflektoren machen das Parklet auch bei schwierigen
+        Witterungsverhältnissen gut sichtbar. Versichert ist das Parklet durch eine Mitgliedschaft
+        bei Tilia e. V..</p></div> <div class="block"><h2>Was wollen wir erreichen?</h2> <p>Mit den ersten Parklets in Stuttgart sind neue Möglichkeiten zur Nutzung des
+        öffentlichen Raums sichtbar geworden. Wir wollen mit dem Parklet Katharinenstraße
+        3.0 zur Aufwertung des Stadtbildes beitragen, einen sicheren Begegnungsort
+        schaffen und einen Ort entstehen lassen, der zur Entspannung und zum Austausch
+        einlädt.</p></div></section></main>`);function h(e){var n=u(),i=s(n);d(i),a(4),l(n),r(e,n)}function f(e){h(e)}export{f as component};
